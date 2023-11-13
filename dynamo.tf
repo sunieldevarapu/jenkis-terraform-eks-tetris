d027a255c18eeb3a090f123bdc0cf29bf765bbc2
sunieldevarapu <65952930+sunieldevarapu@users.noreply.github.com>
2023-11-13 23:05:50 +0530

    Update dynamo.tf

--- dynamo.tf
+++ dynamo.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "dynamodb_table_terraform_state_lock" {
-  name           = "terraform-eks-table1019"
+  name           = "jenkis-tetris-terraform2793"
   hash_key       = "LockID"
   read_capacity  = 20
   write_capacity = 20

aa09604b1bc7e5080ca1f3c82479edcfd666199f
sunieldevarapu <65952930+sunieldevarapu@users.noreply.github.com>
2023-11-15 10:51:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@
 # }
 
 provider "aws" {
-  AWS_ACCESS_KEY_ID = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  AWS_SECRET_ACCESS_KEY = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  AWS_ACCESS_KEY_ID = "AKIAYNPAWIBSEC43MLH7"
+  AWS_SECRET_ACCESS_KEY = "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"
   region     = "us-east-1"
 }
 
